c5d5bcf7f8aa2836ee1577f0511e883fd3921950
mohammedafsalk <techafs456@gmail.com>
2023-07-04 12:48:55 +0530

    cloudinary added

---
+++ Server/Controllers/serviceCenterController.js
@@ -0,0 +1,11 @@
+import ServiceCenterModel from "../Models/serviceCenterModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function serviceCenterSignup(req,res){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
